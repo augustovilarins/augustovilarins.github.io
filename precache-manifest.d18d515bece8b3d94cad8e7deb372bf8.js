53c46a5d2a8a7f330c39004a947542bfb0e14f80
augustovilarins <augusto.vilarins@gmail.com>
2020-06-30 18:14:16 -0300

    Updates

---
+++ precache-manifest.d18d515bece8b3d94cad8e7deb372bf8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18da20dfd56080cb87a1c1c5cb44105e",
+    "url": "portifolio/index.html"
+  },
+  {
+    "revision": "5b75ef4a3adeece7389e",
+    "url": "portifolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "df30cc13386469af1008",
+    "url": "portifolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "2488867d08155e3d3ada",
+    "url": "portifolio/static/css/main.3240214e.chunk.css"
+  },
+  {
+    "revision": "e79c7a42d90c95b5ea53",
+    "url": "portifolio/static/js/2.cd32b882.chunk.js"
+  },
+  {
+    "revision": "5b75ef4a3adeece7389e",
+    "url": "portifolio/static/js/3.96fa82bb.chunk.js"
+  },
+  {
+    "revision": "df30cc13386469af1008",
+    "url": "portifolio/static/js/4.9f149c84.chunk.js"
+  },
+  {
+    "revision": "2488867d08155e3d3ada",
+    "url": "portifolio/static/js/main.294b066a.chunk.js"
+  },
+  {
+    "revision": "bc3c72dbd4cb04e801c8",
+    "url": "portifolio/static/js/runtime-main.1053a648.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "portifolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "portifolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "portifolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "portifolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "portifolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "portifolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "519718dc963e45a9f880f076ab3ba749",
+    "url": "portifolio/static/media/guto.519718dc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "portifolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "portifolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "portifolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "portifolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
